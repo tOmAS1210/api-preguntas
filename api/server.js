20a9a885bd04f0f8c17768e50f7f5efc9247955b
tOmAS1210 <tomasaguilar156@gmail.com>
2024-10-17 11:58:06 -0300

    commit fixed path

--- api/server.js
+++ api/server.js
@@ -5,19 +5,22 @@ const PORT = 3000;
 app.use(cors());
 
 const path = require("path");
-app.use("/fotos_juegos", express.static(path.join(__dirname, "fotos_juegos")));
 app.use(
-  "/fotos_entretenimiento",
+  "../fotos_juegos",
+  express.static(path.join(__dirname, "fotos_juegos"))
+);
+app.use(
+  "../fotos_entretenimiento",
   express.static(path.join(__dirname, "fotos_entretenimiento"))
 );
 app.use(
-  "/fotos_general",
+  "../fotos_general",
   express.static(path.join(__dirname, "fotos_general"))
 );
 
-const preguntasGeneral = require("./preguntasGeneral.json");
-const preguntasEntretenimiento = require("./preguntasEntretenimiento.json");
-const preguntasJuegos = require("./preguntasJuegos.json");
+const preguntasGeneral = require("../preguntasGeneral.json");
+const preguntasEntretenimiento = require("../preguntasEntretenimiento.json");
+const preguntasJuegos = require("../preguntasJuegos.json");
 
 function mezclarPreguntas(array) {
   for (let i = array.length - 1; i > 0; i--) {
